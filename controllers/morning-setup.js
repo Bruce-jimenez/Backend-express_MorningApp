9214a2ed1a747f52a562c2ab70d0ba7462e1b092
Bruce-jimenez <jimenezbruce2022@gmail.com>
2023-01-20 11:22:05 -0500

    Figure out how the routing works in the controllers

--- controllers/morning-setup.js
+++ controllers/morning-setup.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const morningSetup = require('../models/morning-setup');
+const MorningLog = require('../models/morning-setup');
 
 
 //Index
-router.get('/log', (req, res) =>{
-    morningSetup.find({}, (err, foundLog) =>{
+router.get('/', (req, res) =>{
+    MorningLog.find({}, (err, foundLog) =>{
         res.json(foundLog);
     });
 });
 
 //Delete
-router.delete('/log/:id', (req, res) => {
-    morningSetup.findByIdAndRemove(req.params.id, (err, deletedLog) =>{
+router.delete('/:id', (req, res) => {
+    MorningLog.findByIdAndRemove(req.params.id, (err, deletedLog) =>{
         res.json(deletedLog);
     });
 });
 
 //Updated
-router.put('/log/:id', (req,res) => {
-    morningSetup.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedLog) => {
+router.put('/:id', (req,res) => {
+    MorningLog.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedLog) => {
         res.json(updatedLog);
     });
 });
 
 //Create
-router.post('/log', (req, res) => {
-    morningSetup.create(req.body, (err, createdLog) => {
+router.post('/', (req, res) => {
+    MorningLog.create(req.body, (err, createdLog) => {
         res.json(createdLog);
     });
 });
 
 //Show
 router.get('/log/:id', (req,res) => {
-    morningSetup.findById(req.params.id, (err, foundLog) => {
+    MorningLog.findById(req.params.id, (err, foundLog) => {
         res.json(foundLog);
     });
 });
--- controllers/wakeup.js
+++ controllers/wakeup.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const wakeLogs = require('../models/wakeup')
+const wakeup = require('../models/wakeup')
 
 //Index
-router.get('/wake', (req, res) => {
-    wakeLogs.find({}, (err, foundWakeLog) => {
-        res.json(foundWakeLogs);
+router.get('/wake/', (req, res) => {
+    wakeup.find({}, (err, foundWakeLog) => {
+        res.json(foundWakeLog);
     });
 });
 
 //Delete
 router.delete('/wake/:id', (req, res) => {
-    wakeLogs.findByIdAndRemove(req.params.id, (err, deletedWakeLog) => {
+    wakeup.findByIdAndRemove(req.params.id, (err, deletedWakeLog) => {
         res.json(deletedWakeLog);
     });
 });
 
 //Updated
 router.put('/wake/:id', (req,res) => {
-    wakeLogs.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedWakeLogs) => {
+    wakeup.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedWakeLogs) => {
         res.json(updatedWakeLogs);
     });
 });
 
 //Create
-router.post('/wake', (req, res) => {
-    wakeLogs.create(req.body, (err, createdWakeLogs) => {
+router.post('/wake/', (req, res) => {
+    wakeup.create(req.body, (err, createdWakeLogs) => {
         res.json(createdWakeLogs);
     });
 });
 
 //Show
 router.get('/wake/:id', (req, res) => {
-    wakeLogs.findByID(req.params.id, (err, foundWakeLogs) => {
+    wakeup.findByID(req.params.id, (err, foundWakeLogs) => {
         res.json(foundWakeLogs);
     });
 });
